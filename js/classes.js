737d7b6abec68d5e4db48c958a9425b20638fd1c
dargavavid <dargavavid@gmail.com>
2017-11-13 03:55:20 +0100

    Create div representation of cards, implement basic animation for rolling hands

--- js/classes.js
+++ js/classes.js
@@ -1,11 +1,50 @@
 class Card {
-    constructor(value, suite, name = "", x = 0, y = 0, vx = 0, vy = 0) {
+    constructor(value, suite, name = "") {
         this.value = value;
         this.suite = suite;
         this.name = name;
+    }
+}
+
+class DisplayCard extends Card {
+    constructor(value, suite, name, x, y, width, height, vx = 0, vy = 0, targetX = 0, targetY = 0) {
+        super(value, suite, name);
+        this.originalX = x;
+        this.originalY = y;
+        this.targetX = targetX;
+        this.targetY = targetY;
         this.x = x;
         this.y = y;
+        this.width = width;
+        this.height = height;
         this.vx = vx;
         this.vy = vy;
+    }
+    render(context) {
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+    move() {
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+    moveToTarget() {
+        this.x += vx;
+        this.y += vy;
+        if (this.x > this.targetX) {
+            this.x = targetX;
+        }
+        if (this.y > this.targetY) {
+            this.y = this.targetY;
+        }
+    }
+    moveToOriginal() {
+        this.x += vx * -1;
+        this.y += vy * -1;
+        if (this.x > this.originalX) {
+            this.x = originalX;
+        }
+        if (this.y > this.originalY) {
+            this.y = this.orignalY;
+        }
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,57 +1,4 @@
-const app = {
-    settings: {
-        maxCardValue: 14,
-    },
-    state: {
-        moneyWagered: 1
-    }
-};
-
-const handOrder = {
-    "nothing": 0,
-    "onepair": 1,
-    "twopairs": 2,
-    "threeofakind": 3,
-    "straight": 4,
-    "flush": 5,
-    "fullhouse": 6,
-    "poker": 7,
-    "straightflush": 8,
-    "royalflush": 9
-};
-
-const multiplierTable = {
-    "nothing": 0,
-    "onepair": 1,
-    "twopairs": 2,
-    "threeofakind": 3,
-    "straight": 4,
-    "flush": 6,
-    "fullhouse": 9,
-    "poker": 25,
-    "straightflush": 50,
-    "royalflush": 250
-};
-
-const valuePatterns = {
-    "abcde": "nothing",
-    "aabcd": "onepair",
-    "aabbc": "twopairs",
-    "aaabc": "threeofakind",
-    "aaabb": "fullhouse",
-    "aaaab": "poker"
-};
-
-const suitePatterns = {
-    "aaaaa": "flush",
-    //LATER ON IMPLEMENT BONUS PATTERNS??
-}
-
-const cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-const cardNames = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"];
-const suiteValues = [1, 2, 3, 4];
-const suiteNames = ["spades", "clubs", "hearts", "diamonds"];
-
+//FUNCTIONS
 function createCard(value, suite, name) {
     return new Card(value, suite, name);
 }
@@ -189,11 +136,205 @@ function calcReward(hand, wagered, mTable) {
 }
 
 //Create function to simulate round.
-function simulateRound() {
-    const deck = createDeck(cardValues, suiteValues, cardNames, suiteNames);
+function simulateRound(deck = createDeck(cardValues, suiteValues, cardNames, suiteNames)) {
     const shuffled = shuffleDeck(deck);
     const hand = pickHand(shuffled);
     const handValue = getHandValue(hand);
     const reward = calcReward(handValue, app.state.moneyWagered, multiplierTable);
     return reward;
 }
+
+//GLOBAL VARIABLES AND FUNCTIONS RELATED TO VISUALS
+const handOrder = {
+    "nothing": 0,
+    "onepair": 1,
+    "twopairs": 2,
+    "threeofakind": 3,
+    "straight": 4,
+    "flush": 5,
+    "fullhouse": 6,
+    "poker": 7,
+    "straightflush": 8,
+    "royalflush": 9
+};
+
+const multiplierTable = {
+    "nothing": 0,
+    "onepair": 1,
+    "twopairs": 2,
+    "threeofakind": 3,
+    "straight": 4,
+    "flush": 6,
+    "fullhouse": 9,
+    "poker": 25,
+    "straightflush": 50,
+    "royalflush": 250
+};
+
+const valuePatterns = {
+    "abcde": "nothing",
+    "aabcd": "onepair",
+    "aabbc": "twopairs",
+    "aaabc": "threeofakind",
+    "aaabb": "fullhouse",
+    "aaaab": "poker"
+};
+
+const suitePatterns = {
+    "aaaaa": "flush",
+    //LATER ON IMPLEMENT BONUS PATTERNS??
+}
+
+const cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+const cardNames = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"];
+const suiteValues = [1, 2, 3, 4];
+const suiteNames = ["spades", "clubs", "hearts", "diamonds"];
+
+const app = {
+    canvas: document.querySelector("#canvas"),
+    ctx: this.canvas.getContext("2d"),
+    cardDivs: document.querySelectorAll(".card"),
+    deck: createDeck(cardValues, suiteValues, cardNames, suiteNames),
+    hand: [],
+    displayCards: [],
+    settings: {
+        maxCardValue: 14,
+        rollAnimDuration: 1000,
+    },
+    state: {
+        moneyWagered: 1,
+        rollStartDate: null,
+        isRolling: false,
+        movedCardsOut: false,
+        movedCardsIn: false,
+    },
+};
+
+function getCurrentDate() {
+    return Date.now();
+}
+
+function setRoundStartDate() {
+    app.state.rollStartDate = getCurrentDate();
+}
+
+function toggleCardHideShow() {
+    app.cardDivs.forEach(cardDiv => {
+        cardDiv.classList.toggle("cardHide");
+        cardDiv.classList.toggle("cardShow");
+    });
+}
+
+function setCardDivValues(values) {
+    for (let i = 0; i < values.length; i++) {
+        app.cardDivs[i].innerText = values[i];
+    }
+}
+
+function detectAnimationEnd(start, duration) {
+    const now = getCurrentDate();
+    if (now - duration > start) {
+        return true;
+    }else {
+        return false;
+    }
+}
+
+function animateCardsInOut() {
+    const now = getCurrentDate();
+    const currentDuration = now - app.state.rollStartDate;
+    if (!app.state.movedCardsOut && currentDuration <= app.settings.rollAnimDuration/2) {
+        toggleCardHideShow();
+        app.state.movedCardsOut = true;
+    }
+    if (!app.state.movedCardsIn && currentDuration >= app.settings.rollAnimDuration/2) {
+        toggleCardHideShow();
+        app.state.movedCardsIn = true;
+        setCardDivValues(app.hand.map(cardObj => cardObj.value));
+    }
+    if (detectAnimationEnd(app.state.rollStartDate, app.settings.rollAnimDuration)) {
+        app.state.isRolling = false;
+    }
+}
+
+function roll() {
+    if (!app.state.isRolling) {
+        app.state.rollStartDate = getCurrentDate();
+        app.state.movedCardsOut = false;
+        app.state.movedCardsIn = false;
+        app.state.isRolling = true;
+
+        app.deck = shuffleDeck(app.deck);
+        app.hand = pickHand(app.deck);
+        
+    }else {
+        console.log("still rolling");
+    }
+}
+
+// function replaceSelectedCards(selected, deck) {
+//     const remainingDeck = [selected, ...deck.slice(5)];
+//     const remainingDeckShuffled = shuffleDeck(remainingDeck);
+
+// }
+
+function mainLoop(time = 0) {
+    window.requestAnimationFrame(mainLoop);
+    if (app.state.isRolling) {
+        animateCardsInOut();
+    }
+}
+
+mainLoop();
+// function createDisplayCards(canvas, n = 5) {
+//     const displayCards = [];
+//     const horizontalGap = 10, verticalGap = 50;
+//     const cardWidth = (canvas.width - (n + 1) * horizontalGap) / n;
+//     const cardHeight = canvas.height - verticalGap * 2;
+//     let currentX = horizontalGap, currentY = verticalGap;
+//     let displayCard;
+//     for (let i = 0, vy = 10; i < n; i++, vy += 1) {
+//         displayCard = new DisplayCard(0, 0, 0, currentX, currentY, cardWidth, cardHeight, 0, vy);
+//         displayCards.push(displayCard);
+//         currentX += horizontalGap + cardWidth;
+//     }
+//     return displayCards;
+// }
+
+// function renderDisplayCards(canvas, context) {
+//     const displayCards = createDisplayCards(canvas);
+//     displayCards.forEach(displayCard => displayCard.render(context));
+// }
+
+// function setAppDisplayCards() {
+//     app.displayCards = createDisplayCards(app.canvas);
+// }
+
+// function renderAppDisplayCards() {
+//     app.displayCards.forEach(displayCard => displayCard.render(app.ctx));
+// }
+
+// function moveAppDisplayCards() {
+//     app.displayCards.forEach(displayCard => displayCard.move());
+// }
+
+// function clearCanvas(canvas, context) {
+//     context.clearRect(0, 0, canvas.width, canvas.height);
+// }
+
+// function mainLoop(time = 0) {
+//     window.requestAnimationFrame(mainLoop);
+//     clearCanvas(app.canvas, app.ctx);
+//     renderAppDisplayCards();
+//     moveAppDisplayCards();
+// }
+
+// function initApp(delay = 500) {
+//     setTimeout(function() {
+//         setAppDisplayCards();
+//         mainLoop();
+//     }, delay);
+// }
+
+
+// initApp();
